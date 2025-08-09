3ae0efb961154683355688e8fae9d4b7dc2885ac
Angel Fernando Dal Nieto <65708300+angeldal0@users.noreply.github.com>
2025-08-09 18:39:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,62 +2,40 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-// Mi endpoint para la consulta "/api/:date?"
-// Sin control de errores
-app.get("/api/:date?", (req, res) => {
-  if (req.params.date) {
-    let dateRes;
-    let date_string = req.params.date;
+// endpoint
+app.get("/api/whoami", (req, res) => {
+  const xff = req.headers['x-forwarded-for'];
+  console.log(xff);
+  const ipaddress = xff ? xff.split(',')[0].trim() : req.ip;
+  const language = req.headers['accept-language'] ? req.headers['accept-language'].split(',')[0] : '';
+  const software = req.headers['user-agent'] || '';
 
-    if (/^\d+$/.test(date_string)) {
-      dateRes = new Date(Number(date_string)); // ó parseInt(date_string, 10)
-    } else {
-      dateRes = new Date(date_string);
-    }
+  res.json({ ipaddress, language, software });
+})
 
-    if (!isNaN(dateRes.getTime())) {
-
-      let unixRes = dateRes.getTime();
-      res.json({
-        unix: unixRes,
-        utc: dateRes.toUTCString()
-      })
-    } else {
-      res.json({error : "Invalid Date"});
-    }
-
-  } else {
-    res.json({
-      unix: new Date().getTime(),
-      utc : new Date().toUTCString()
-    })
-  }
-});
-
-
-// Listen on port set in environment variable or default to 3000
+// listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
