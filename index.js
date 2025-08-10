56de147a54e4d6d713aad21d67dcc2d8661533e1
Angel Fernando Dal Nieto <65708300+angeldal0@users.noreply.github.com>
2025-08-10 13:28:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,34 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+const multer = require("multer");
+
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// middleware de multer
+// Configuración: almacenamiento en memoria
+const upload = multer({ storage: multer.memoryStorage() });
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// endpoint para analizar el archivo
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) return res.status(400).send('No se subió ningún archivo');
 
-// Mi endpoint para la consulta "/api/:date?"
-// Sin control de errores
-app.get("/api/:date?", (req, res) => {
-  if (req.params.date) {
-    let dateRes;
-    let date_string = req.params.date;
-
-    if (/^\d+$/.test(date_string)) {
-      dateRes = new Date(Number(date_string)); // ó parseInt(date_string, 10)
-    } else {
-      dateRes = new Date(date_string);
-    }
-
-    if (!isNaN(dateRes.getTime())) {
-
-      let unixRes = dateRes.getTime();
-      res.json({
-        unix: unixRes,
-        utc: dateRes.toUTCString()
-      })
-    } else {
-      res.json({error : "Invalid Date"});
-    }
-
-  } else {
-    res.json({
-      unix: new Date().getTime(),
-      utc : new Date().toUTCString()
-    })
-  }
-});
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
 });
